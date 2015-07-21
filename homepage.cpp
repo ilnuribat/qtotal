f9494a9975a7dfcfac820d1eeb05837f0c8f52a1
ilnuribat <ilnuribat@gmail.com>
2015-07-21 12:46:18 +0500

    get name, position from ini file, set them to side menu

--- homepage.cpp
+++ homepage.cpp
@@ -4,9 +4,13 @@ homePage::homePage(QQmlApplicationEngine &engine)
 {
     mainQML = engine.rootObjects().value(0);
     engine.rootContext ()->setContextProperty ("home", this);
+    settings = new QSettings("settings.ini", QSettings::IniFormat);
 }
 
 void homePage::loaded ()
 {
     qDebug() << "home page loaded";
+    QObject *sideMenu = mainQML->findChild<QObject*>("sideMenu");
+    sideMenu->setProperty ("name_lastname", settings->value ("name_lastname"));
+    sideMenu->setProperty ("position", settings->value ("position"));
 }
