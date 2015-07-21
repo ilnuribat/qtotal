16092824cab6334c597fc524ee5346ec3ea235ec
ilnuribat <ilnuribat@gmail.com>
2015-07-21 10:54:47 +0500

    init function, that works with cache

--- main.cpp
+++ main.cpp
@@ -3,20 +3,32 @@
 #include "externvars.h"
 #include "homepage.h"
 
+void init(QQmlApplicationEngine &engine)
+{
+    //Логика кэширования. Если человек уже логинился, то
+    //нужно сразу в следующую страницу
+    QObject *mainQML = engine.rootObjects().value(0);
+    QObject *loader = mainQML->findChild<QObject*>("loader");
+    QSettings *settings = new QSettings("settings.ini", QSettings::IniFormat);
+    QString ID = settings->value ("ID").toString ();
+    if (ID.size () > 0) {
+        loader->setProperty ("source", "qrc:/QMLs/Home.qml");
+    }
+    else {
+        loader->setProperty ("source", "qrc:/QMLs/Welcome.qml");
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QQmlApplicationEngine engine;
     engine.load(QUrl("qrc:/main.qml"));
 
-    welcomePage *welcome;
-    welcome = new welcomePage(engine);
+    new welcomePage(engine);
+    new homePage(engine);
 
-
-
-    homePage *home;
-    home = new homePage(engine);
-    engine.rootContext ()->setContextProperty ("home", home);
+    init(engine);
 
     return app.exec();
 }
