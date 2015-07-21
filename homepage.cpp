95738268f97ba88be53ef79a7dd15f737156a22b
ilnuribat <ilnuribat@gmail.com>
2015-07-21 10:55:31 +0500

    setContextProperty is moved to here from main.cpp

--- homepage.cpp
+++ homepage.cpp
@@ -3,5 +3,10 @@
 homePage::homePage(QQmlApplicationEngine &engine)
 {
     mainQML = engine.rootObjects().value(0);
+    engine.rootContext ()->setContextProperty ("home", this);
 }
 
+void homePage::loaded ()
+{
+    qDebug() << "home page loaded";
+}
